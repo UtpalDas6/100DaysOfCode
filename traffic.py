e7980160ace676df0891ee31437b5eaa933ce612
Utpal Das <36782722+UtpalDas6@users.noreply.github.com>
2021-01-06 12:23:15 +0530

    Traffic control system

---
+++ traffic.py
@@ -0,0 +1,50 @@
+#Day1.3
+import emoji
+import time
+from IPython.display import clear_output
+class Direction:
+  def __init__(self,color):
+    self.color = color
+def current(N,E,W,S):
+  clear_output()
+  print('====TRAFFIC====')
+  print('N ',emoji.emojize(':'+N.color+':'))
+  print('E ',emoji.emojize(':'+E.color+':'))
+  print('S ',emoji.emojize(':'+S.color+':'))
+  print('W ',emoji.emojize(':'+W.color+':'))
+def tcs(N,E,W,S):
+  while True:
+    current(N,E,W,S)
+    time.sleep(20)
+    N.color = 'orange_heart'
+    current(N,E,W,S)
+    time.sleep(5)
+    N.color = 'red_circle'
+    E.color = 'green_heart'
+    current(N,E,W,S)
+    time.sleep(20)
+    E.color = 'orange_heart'
+    current(N,E,W,S)
+    time.sleep(5)
+    E.color = 'red_circle'
+    S.color = 'green_heart'
+    current(N,E,W,S)
+    time.sleep(20)
+    S.color = 'orange_heart'
+    current(N,E,W,S)
+    time.sleep(5)
+    S.color = 'red_circle'
+    W.color = 'green_heart'
+    current(N,E,W,S)
+    time.sleep(20)
+    W.color = 'orange_heart'
+    current(N,E,W,S)
+    time.sleep(5)
+    W.color = 'red_circle'
+    N.color = 'green_heart'
+ 
+N = Direction('green_heart')
+E = Direction('red_circle')
+S = Direction('red_circle')
+W = Direction('red_circle')
+tcs(N,E,W,S)
