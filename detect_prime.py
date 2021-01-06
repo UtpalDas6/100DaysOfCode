636d975c6359b0e6434c7511ce21340d3aaee430
Utpal Das <36782722+UtpalDas6@users.noreply.github.com>
2021-01-06 12:20:19 +0530

    Create detect_prime.py

---
+++ detect_prime.py
@@ -0,0 +1,13 @@
+#Day1.1
+#prim_func(1,2,3,4,5,6) => 2,3,5
+def prim_func(lis):
+  op = []
+  for el in lis:
+    factor_count = 0
+    for i in range(1,el+1):
+      if el%i == 0:
+        factor_count+=1
+    if factor_count == 2:
+      op.append(el)
+  return op
+print(prim_func([2,3,4,5,6]))
