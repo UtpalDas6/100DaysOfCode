d5be1bbe89dcf20ee48e3ec4ce31dfd21031f87e
Utpal Das <36782722+UtpalDas6@users.noreply.github.com>
2025-06-08 00:15:52 +0530

    Fix binary conversion for zero input

--- find_binary.py
+++ find_binary.py
@@ -1,9 +1,29 @@
-#Day1.1
-def binary(num):
-  val = ''
-  while num>0:
-    val += str(num%2)
-    num = num//2
-  return val
-num = input("Enter any number: ")
-print("Binary Equivalent: ",binary(int(num))[::-1])
+"""Convert an integer to its binary representation."""
+
+def binary(num: int) -> str:
+    """Return the binary representation of ``num``.
+
+    Parameters
+    ----------
+    num : int
+        The non-negative integer to convert.
+
+    Returns
+    -------
+    str
+        Binary string without leading ``0b`` prefix.
+    """
+
+    if num == 0:
+        return "0"
+
+    val = ""
+    while num > 0:
+        val = str(num % 2) + val
+        num //= 2
+    return val
+
+
+if __name__ == "__main__":
+    num = int(input("Enter any number: "))
+    print("Binary Equivalent: ", binary(num))
